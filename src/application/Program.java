7d8c005adc72be70cffd6c2e3daf6373a19a1182
DanyelBM <e.danyel@gmail.com>
2019-09-26 00:45:14 -0300

    Destacando movimentos possíveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,10 @@ public class Program {
 				System.out.println();
 				System.out.print("Origem: ");
 				ChessPosition source = UI.readChessPosition(scan);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 			
 				System.out.print("Destino: ");
 				ChessPosition target = UI.readChessPosition(scan);
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
 	private int row;
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Erro ao instanciar ChessPosition. Valores válidos são de a1 a h8.");
+			throw new ChessException("Erro ao instanciar ChessPosition. Valores validos sao de a1 a h8.");
 		}
 		this.column = column;
 		this.row = row;
